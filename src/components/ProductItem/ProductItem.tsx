8109116fd36626af14c40c927a46d940405653bd
Pooria Setayesh <pooriaset@yahoo.com>
2024-03-21 02:31:35 +0330

    feat: add basic product item component

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,17 +1,17 @@
 import { Card, CardContent, CardMedia } from "@mui/material";
-import Image from "next/image";
+
 import React from "react";
+import Image from "../common/Image";
 
 const ProductItem = () => {
   return (
     <Card>
       <CardContent>
-        <CardMedia
-          component={Image}
-          height={80}
-          sx={{ width: 151 }}
-          image="/static/images/cards/live-from-space.jpg"
-          alt="Live from space album cover"
+        <Image
+          height={100}
+          width={100}
+          src="/assets/images/placeholders/100x100.jpg"
+          alt="Product Image"
         />
       </CardContent>
     </Card>
---
+++ src/components/common/Image.tsx
@@ -0,0 +1,31 @@
+import Skeleton from "@mui/material/Skeleton";
+import NextImage, { ImageProps } from "next/image";
+import { FC, useState } from "react";
+
+const Image: FC<ImageProps> = ({ width, height, ...props }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  const handleOnLoad = () => {
+    setLoaded(true);
+  };
+
+  return (
+    <>
+      {!loaded && (
+        <Skeleton variant="rectangular" width={width} height={height} />
+      )}
+      <NextImage
+        width={width}
+        height={height}
+        {...props}
+        onLoad={handleOnLoad}
+        style={{
+          visibility: loaded ? "visible" : "hidden",
+          height: loaded ? "auto" : 0,
+        }}
+      />
+    </>
+  );
+};
+
+export default Image;
