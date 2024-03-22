5344093abd528db1e3b52ee18a8a8e0a95060051
Pooria Setayesh <pooriaset@yahoo.com>
2024-03-22 13:03:36 +0330

    chore: add volta to project

--- src/components/ColumnFilters/ColumnFilters.tsx
+++ src/components/ColumnFilters/ColumnFilters.tsx
@@ -1,7 +1,36 @@
-import { Card, CardContent } from "@mui/material";
-import React from "react";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import {
+  Card,
+  CardContent,
+  Collapse,
+  Divider,
+  List,
+  ListItemButton,
+  ListItemText,
+  Switch,
+  styled,
+} from "@mui/material";
+import { useState } from "react";
+
+const ListItem = styled(ListItemButton)(({ theme }) => ({
+  "&:hover": {
+    backgroundColor: "transparent",
+  },
+  py: 1,
+}));
+
+const Title = styled("span")(({ theme }) => ({
+  fontSize: theme.typography.body2.fontSize,
+  fontWeight: 700,
+}));
 
 const ColumnFilters = () => {
+  const [open, setOpen] = useState(true);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <Card
       sx={{
@@ -9,7 +38,28 @@ const ColumnFilters = () => {
         top: 130,
       }}
     >
-      <CardContent>Filters</CardContent>
+      <CardContent>
+        <List>
+          <ListItem disableGutters onClick={handleClick} disableRipple>
+            <ListItemText primary={<Title>دسته‌بندی</Title>} />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <List component="div">
+              <ListItem disableRipple>
+                <ListItemText primary={<Title>دسته‌بندی اول</Title>} />
+              </ListItem>
+            </List>
+          </Collapse>
+
+          <Divider />
+
+          <ListItem disableGutters disableRipple>
+            <ListItemText primary={<Title>فقط کالاهای موجود</Title>} />
+            <Switch />
+          </ListItem>
+        </List>
+      </CardContent>
     </Card>
   );
 };
