f50d9e278656776381a880f3628a5056e1024b92
Pooria Setayesh <pooriaset@yahoo.com>
2024-03-26 19:16:33 +0330

    chore: remove pnp.cjs and add some yarn folders like cache to gitignore

--- src/components/Header/components/DesktopView.tsx
+++ src/components/Header/components/DesktopView.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import { Suspense } from "react";
 import BottomSection from "./BottomSection";
 import TopSection from "./TopSection";
 
 const DesktopView = () => {
   return (
-    <>
+    <Suspense>
       <TopSection />
       <BottomSection />
-    </>
+    </Suspense>
   );
 };
 
