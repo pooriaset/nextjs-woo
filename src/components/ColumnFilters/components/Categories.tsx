48a2431313cbf8a17b16c1548bdf7275bd466b58
Pooria Setayesh <pooriaset@yahoo.com>
2024-04-12 02:06:51 +0330

    feat: convert the categories component to collapsible

--- src/components/ColumnFilters/components/Categories.tsx
+++ src/components/ColumnFilters/components/Categories.tsx
@@ -1,8 +1,9 @@
-import React, { FC } from 'react';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+import { Collapse, List, ListItemText } from '@mui/material';
+import { FC, MouseEventHandler, useState } from 'react';
 import { Options } from '../types';
-import { List, ListItemText } from '@mui/material';
-import { Title } from './Title';
 import { ListItem } from './ListItem';
+import { Title } from './Title';
 
 export interface CategoriesProps {
   options: Options;
@@ -13,10 +14,26 @@ const Categories: FC<CategoriesProps> = ({ options, parentId }) => {
   const _options =
     options?.filter((option) => option.parentId === parentId) ?? [];
 
+  const [open, setOpen] = useState<Record<number, boolean>>({});
+
+  const handleClickOnItem = (id: number) => {
+    const func: MouseEventHandler<HTMLDivElement> = (event) => {
+      setOpen((prevState) => {
+        const status = !prevState[id];
+        return {
+          ...prevState,
+          [id]: status,
+        };
+      });
+    };
+    return func;
+  };
+
   if (!_options.length) {
     return null;
   }
   return _options.map((option) => {
+    const hasChildren = options?.some((child) => child.parentId === option.id);
     return (
       <List
         dense
@@ -27,10 +44,15 @@ const Categories: FC<CategoriesProps> = ({ options, parentId }) => {
         }}
         disablePadding
       >
-        <ListItem dense disableRipple>
+        <ListItem dense disableRipple onClick={handleClickOnItem(option.id)}>
           <ListItemText primary={<Title>{option.name}</Title>} />
+          {hasChildren && (
+            <>{open[option.id] ? <ExpandLess /> : <ExpandMore />}</>
+          )}
         </ListItem>
-        <Categories options={options} parentId={option.id} />
+        <Collapse timeout="auto" in={open[option.id] ?? false} unmountOnExit>
+          <Categories options={options} parentId={option.id} />
+        </Collapse>
       </List>
     );
   });
