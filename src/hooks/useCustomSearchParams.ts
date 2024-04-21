63ccea67ed7e23ac39ea266102807e4770ea2620
Pooria Setayesh <pooriaset@yahoo.com>
2024-04-21 09:45:54 +0330

    fix: use useRouter of next-intl instead of default useRouter

--- src/hooks/useCustomSearchParams.ts
+++ src/hooks/useCustomSearchParams.ts
@@ -1,7 +1,8 @@
 'use client';
 
+import { useRouter } from '@/navigation';
 import { sortOptions } from '@/static/sortOptions';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 enum SearchPageParams {
   Sort = 'sort',
