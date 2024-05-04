20f086bff3d17b65dd38e18018f176759faac660
Pooria Setayesh <pooriaset@yahoo.com>
2024-05-04 17:14:02 +0330

    feat: set correct link for best selling page

--- src/components/Header/components/BottomSection.tsx
+++ src/components/Header/components/BottomSection.tsx
@@ -2,14 +2,19 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import { useTranslations } from 'next-intl';
 import { Link as NextLink } from '@/navigation';
+import { bestSellingSortOption } from '@/static/sortOptions';
 
 const BottomSection = () => {
   const t = useTranslations();
+
   const pages = [
     { label: t('header.navigation.products'), href: '/search' },
     { label: t('header.navigation.categories'), href: '/categories' },
-    { label: t('header.navigation.promotions'), href: '/best-selling' },
-    { label: t('header.navigation.bestSelling'), href: '/promotion-center' },
+    {
+      label: t('header.navigation.bestSelling'),
+      href: `/search?sort=${bestSellingSortOption.key}`,
+    },
+    { label: t('header.navigation.promotions'), href: '/promotion-center' },
   ];
 
   return (
