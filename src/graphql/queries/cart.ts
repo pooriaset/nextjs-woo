17dc5dc0edf4f1acda8b0562c5ab72f246d10358
Pooria Setayesh <pooriaset@yahoo.com>
2024-05-18 15:04:21 +0330

    chore: add cart query

---
+++ src/graphql/queries/cart.ts
@@ -0,0 +1,84 @@
+import { gql } from '@apollo/client';
+
+export const GET_CART_QUERY = gql`
+  query GET_CART {
+    cart {
+      contents {
+        nodes {
+          key
+          product {
+            node {
+              id
+              databaseId
+              name
+              description
+              type
+              onSale
+              slug
+              averageRating
+              reviewCount
+              image {
+                id
+                sourceUrl
+                srcSet
+                altText
+                title
+              }
+              galleryImages {
+                nodes {
+                  id
+                  sourceUrl
+                  srcSet
+                  altText
+                  title
+                }
+              }
+            }
+          }
+          variation {
+            node {
+              id
+              databaseId
+              name
+              description
+              type
+              onSale
+              price
+              regularPrice
+              salePrice
+              image {
+                id
+                sourceUrl
+                srcSet
+                altText
+                title
+              }
+              attributes {
+                nodes {
+                  id
+                  name
+                  value
+                }
+              }
+            }
+          }
+          quantity
+          total
+          subtotal
+          subtotalTax
+        }
+      }
+
+      subtotal
+      subtotalTax
+      shippingTax
+      shippingTotal
+      total
+      totalTax
+      feeTax
+      feeTotal
+      discountTax
+      discountTotal
+    }
+  }
+`;
