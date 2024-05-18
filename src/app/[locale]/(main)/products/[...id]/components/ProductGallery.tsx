a803720f8ec1d82abfb749565c92befbae75cd39
Pooria Setayesh <pooriaset@yahoo.com>
2024-05-18 23:19:06 +0330

    feat: add tooltip to product image

--- src/app/[locale]/(main)/products/[...id]/components/ProductGallery.tsx
+++ src/app/[locale]/(main)/products/[...id]/components/ProductGallery.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import Image from '@/components/common/Image';
-import { MoreHorizOutlined } from '@mui/icons-material';
-import { Box } from '@mui/material';
+import { FullscreenOutlined } from '@mui/icons-material';
+import { Box, Tooltip } from '@mui/material';
+import { useTranslations } from 'next-intl';
 import { FC, useState } from 'react';
 import { Product } from '../../types/common';
 import ProductGalleryDialog from './ProductGalleryDialog';
@@ -16,6 +17,8 @@ export interface ProductGalleryProps {
   value?: ProductImages;
 }
 const ProductGallery: FC<ProductGalleryProps> = ({ value = [] }) => {
+  const t = useTranslations();
+
   const [openDialog, setOpenDialog] = useState(false);
 
   if (!value.length) {
@@ -49,53 +52,57 @@ const ProductGallery: FC<ProductGalleryProps> = ({ value = [] }) => {
         {value?.map((item, index) => {
           const isLast = value.length - 1 == index;
           return (
-            <Box
-              key={index}
-              sx={{
-                p: 0.5,
-                border: '1px solid',
-                borderColor: (theme) => theme.palette.divider,
-                borderRadius: 1,
-                width: 72,
-                height: 72,
-                position: 'relative',
-              }}
+            <Tooltip
+              title={isLast ? t('pages.product.tooltips.fullScreen') : null}
+              key={item.id}
             >
-              <Image
-                onClick={handleClickOnImage}
-                draggable={false}
-                width={72}
-                height={72}
-                alt={item.altText}
-                src={item.sourceUrl}
-                style={{
-                  objectFit: 'contain',
-                  cursor: 'pointer',
-                  userSelect: 'none',
-                  maxWidth: '100%',
-                  maxHeight: '100%',
-                  ...(isLast
-                    ? {
-                        filter: 'blur(4px)',
-                        opacity: 0.5,
-                      }
-                    : {}),
+              <Box
+                sx={{
+                  p: 0.5,
+                  border: '1px solid',
+                  borderColor: (theme) => theme.palette.divider,
+                  borderRadius: 1,
+                  width: 72,
+                  height: 72,
+                  position: 'relative',
                 }}
-              />
-              {isLast && (
-                <MoreHorizOutlined
-                  fontSize="large"
-                  color="disabled"
-                  sx={{
-                    pointerEvents: 'none',
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%,-50%)',
+              >
+                <Image
+                  onClick={handleClickOnImage}
+                  draggable={false}
+                  width={72}
+                  height={72}
+                  alt={item.altText}
+                  src={item.sourceUrl}
+                  style={{
+                    objectFit: 'contain',
+                    cursor: 'pointer',
+                    userSelect: 'none',
+                    maxWidth: '100%',
+                    maxHeight: '100%',
+                    ...(isLast
+                      ? {
+                          filter: 'blur(4px)',
+                          opacity: 0.5,
+                        }
+                      : {}),
                   }}
                 />
-              )}
-            </Box>
+                {isLast && (
+                  <FullscreenOutlined
+                    fontSize="large"
+                    color="disabled"
+                    sx={{
+                      pointerEvents: 'none',
+                      position: 'absolute',
+                      top: '50%',
+                      left: '50%',
+                      transform: 'translate(-50%,-50%)',
+                    }}
+                  />
+                )}
+              </Box>
+            </Tooltip>
           );
         })}
       </Box>
