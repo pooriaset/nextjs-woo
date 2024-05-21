f564df5fa48ff7643b677cf02c6bef8ea898b8ca
Pooria Setayesh <pooriaset@yahoo.com>
2024-05-21 09:38:53 +0330

    feat: add homepage skeleton

--- src/app/[locale]/(main)/loading.tsx
+++ src/app/[locale]/(main)/loading.tsx
@@ -1,5 +1,23 @@
-import LoadingComponent from '@/components/common/LoadingComponent';
+import { Box, Grid, Skeleton } from '@mui/material';
 
-const Loading = () => <LoadingComponent />;
+const Loading = () => {
+  return (
+    <>
+      <Skeleton variant="rectangular" height={350} />
+      <Grid container spacing={1}>
+        {new Array(4).fill(1).map((item, index) => {
+          return (
+            <Grid key={index} item xs={12} md={6} lg={4} xl={3}>
+              <Box>
+                <Skeleton variant="rectangular" height={120} />
+                <Skeleton variant="text" width={60} />
+              </Box>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
 
 export default Loading;
