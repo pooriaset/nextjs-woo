a7da6fa68605ac3e23cf679980f3b03941cd6c75
Pooria Setayesh <pooriaset@yahoo.com>
2024-12-03 11:57:16 +0330

    feat: initialize profile page

---
+++ src/app/[locale]/(main)/profile/components/Menu.tsx
@@ -0,0 +1,14 @@
+import { Box, Card, CardContent, CardActions, Button } from '@mui/material';
+import React from 'react';
+
+const Menu = () => {
+  return (
+    <Box sx={{ width: 250 }}>
+      <Card variant="outlined">
+        <CardContent>Menu</CardContent>
+      </Card>
+    </Box>
+  );
+};
+
+export default Menu;
