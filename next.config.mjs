b0ccecd64bab058e23d9ee0916ed1776b70d35f9
Pooria Setayesh <pooriaset@yahoo.com>
2024-12-09 01:44:33 +0330

    ci: add docker file of project

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ import nextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = nextIntlPlugin('./src/i18n.ts');
 
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: false,
   images: {
     remotePatterns: [
