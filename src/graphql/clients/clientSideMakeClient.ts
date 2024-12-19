b9be40c2b02b609a7a140bfe39ab42e2c3b369ea
Pooria Setayesh <pooriaset@yahoo.com>
2024-12-19 22:55:30 +0330

    refactor: store woo token in the cookie

--- src/graphql/clients/clientSideMakeClient.ts
+++ src/graphql/clients/clientSideMakeClient.ts
@@ -6,70 +6,26 @@ import {
 } from '@apollo/experimental-nextjs-app-support/ssr';
 import { createErrorLink } from '../utils';
 import { PUBLIC_GATEWAY_URL } from '@/config/app';
+import Cookies from 'js-cookie';
 
-const SEVEN_DAYS = 7 * 24 * 60 * 60 * 1000; // 7 days in milliseconds
-const WOO_SESSION_KEY = 'woo-session';
-
-/**
- * Woo Session setter operation.
- * If we have a session token in localStorage, add it to the GraphQL request as a Session header.
- */
-export const wooSessionSetter = new ApolloLink((operation, forward) => {
-  /**
-   * If session data exist in local storage, set value as session header.
-   * Here we also delete the session if it is older than 7 days
-   */
-  const localStorageValue = localStorage.getItem(WOO_SESSION_KEY);
-  const sessionData = localStorageValue ? JSON.parse(localStorageValue) : null;
-
-  if (sessionData && sessionData.token && sessionData.createdTime) {
-    const { token, createdTime } = sessionData;
-
-    // Check if the token is older than 7 days
-    if (Date.now() - createdTime > SEVEN_DAYS) {
-      // If it is, delete it
-      localStorage.removeItem(WOO_SESSION_KEY);
-      localStorage.setItem('woocommerce-cart', JSON.stringify({}));
-    } else {
-      // If it's not, use the token
-      operation.setContext(() => ({
-        headers: {
-          'woocommerce-session': `Session ${token}`,
-        },
-      }));
-    }
-  }
-
-  return forward(operation);
-});
+export const WOO_SESSION_KEY = 'woo-session';
 
 /**
- * Woo Session updater operation.
- *
- * This catches the incoming session token and stores it in localStorage, for future GraphQL requests.
+ * Apollo Link to update the WooCommerce session token.
+ * This link captures the session token from the response headers and stores it in cookies.
  */
 export const wooSessionUpdater = new ApolloLink((operation, forward) =>
   forward(operation).map((response) => {
-    /**
-     * Check for session header and update session in local storage accordingly.
-     */
     const context = operation.getContext();
-    const {
-      response: { headers },
-    } = context;
-
-    const session = headers.get('woocommerce-session');
+    const session = context.response?.headers.get('woocommerce-session');
 
     if (session) {
-      if ('false' === session) {
-        // Remove session data if session destroyed.
-        localStorage.removeItem(WOO_SESSION_KEY);
-        // Update session new data if changed.
-      } else if (!localStorage.getItem(WOO_SESSION_KEY)) {
-        localStorage.setItem(
-          WOO_SESSION_KEY,
-          JSON.stringify({ token: session, createdTime: Date.now() }),
-        );
+      if (session === 'false') {
+        // Remove session data if the session is destroyed
+        Cookies.remove(WOO_SESSION_KEY);
+      } else if (!Cookies.get(WOO_SESSION_KEY)) {
+        // Store new session token if it doesn't exist
+        Cookies.set(WOO_SESSION_KEY, session, { expires: 7 });
       }
     }
 
@@ -77,22 +33,23 @@ export const wooSessionUpdater = new ApolloLink((operation, forward) =>
   }),
 );
 
+// Create an HTTP link for Apollo Client
 const httpLink = new HttpLink({
   uri: PUBLIC_GATEWAY_URL,
 });
 
+/**
+ * Function to create an Apollo Client instance with SSR support.
+ */
 export const makeClient = () => {
   return new NextSSRApolloClient({
     ssrMode: true,
     connectToDevTools: true,
     cache: new NextSSRInMemoryCache(),
     link: from([
-      wooSessionSetter,
       wooSessionUpdater,
       createErrorLink(),
-      new SSRMultipartLink({
-        stripDefer: true,
-      }),
+      new SSRMultipartLink({ stripDefer: true }),
       httpLink,
     ]),
   });
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import nextIntlMiddleware from 'next-intl/middleware';
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { PUBLIC_GATEWAY_URL, protectedRoutes } from './config/app';
+import { WOO_SESSION_KEY } from './graphql/clients/clientSideMakeClient';
 
 const intlMiddleware = (request: NextRequest) =>
   Promise.resolve(
@@ -18,6 +19,11 @@ const intlMiddleware = (request: NextRequest) =>
 async function middleware(request: NextRequestWithAuth) {
   const { pathname } = request.nextUrl;
   request.headers.set('x-pathname', pathname);
+
+  const wooToken = request.cookies.get(WOO_SESSION_KEY)?.value;
+  if (wooToken) {
+    request.headers.set('woocommerce-session', `Session ${wooToken}`);
+  }
 
   if (pathname.startsWith(PUBLIC_GATEWAY_URL)) {
     request.headers.delete('cookie');
