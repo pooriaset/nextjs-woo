4747661c2fc5d9c0754dcadfefb883533389c204
Pooria Setayesh <pooriaset@yahoo.com>
2024-12-20 13:22:55 +0330

    feat: add cookie updater

--- src/graphql/clients/clientSideMakeClient.ts
+++ src/graphql/clients/clientSideMakeClient.ts
@@ -1,5 +1,5 @@
 import { PUBLIC_GATEWAY_URL } from '@/config/app';
-import { ApolloClient, HttpLink, from } from '@apollo/client';
+import { ApolloClient, ApolloLink, HttpLink, from } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import {
   NextSSRApolloClient,
@@ -22,7 +22,7 @@ const client = new ApolloClient({
   link: from([httpLink]),
 });
 
-const createSessionLink = () => {
+const sessionCreatorLink = () => {
   return setContext(async ({ context: { headers: currentHeaders } = {} }) => {
     const headers = { ...currentHeaders };
 
@@ -48,13 +48,31 @@ const createSessionLink = () => {
   });
 };
 
+const sessionUpdaterLink = new ApolloLink((operation, forward) =>
+  forward(operation).map((response) => {
+    const context = operation.getContext();
+    const {
+      response: { headers },
+    } = context;
+
+    const session = headers.get('woocommerce-session');
+
+    if (session && !session.includes('Session')) {
+      Cookies.set(WOO_SESSION_KEY, session, { expires: 7 });
+    }
+
+    return response;
+  }),
+);
+
 export const makeClient = () => {
   return new NextSSRApolloClient({
     ssrMode: true,
     connectToDevTools: true,
     cache: new NextSSRInMemoryCache(),
     link: from([
-      createSessionLink(),
+      sessionCreatorLink(),
+      sessionUpdaterLink,
       createErrorLink(),
       new SSRMultipartLink({ stripDefer: true }),
       httpLink,
