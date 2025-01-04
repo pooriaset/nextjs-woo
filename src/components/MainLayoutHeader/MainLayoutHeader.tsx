bcfc15785e406f4b2987eebaaae3f9ad28355a6d
Pooria Setayesh <pooriasetayesh@gmail.com>
2025-01-04 11:09:23 +0330

    chore: make header client

--- src/components/MainLayoutHeader/MainLayoutHeader.tsx
+++ src/components/MainLayoutHeader/MainLayoutHeader.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import { ISliderItem } from '@/components/MainSlider/types';
-import { getClient } from '@/graphql/clients/serverSideClient';
 import { GET_TOP_BANNER } from '@/graphql/queries/sliders';
 import { GetTopBannerQuery } from '@/graphql/types/graphql';
+import { useQuery } from '@apollo/client';
 import { Container } from '@mui/material';
 import { Header } from '../Header';
 import { DesktopHeader } from '../Header/components';
@@ -9,31 +11,23 @@ import MobileHeader from '../Header/components/MobileHeader';
 import TopBanner from '../Header/components/TopBanner';
 import { DesktopView, MobileView } from '../ResponsiveDesign';
 
-const getTopBanner = async () => {
-  const { data } = await getClient().query<GetTopBannerQuery>({
-    query: GET_TOP_BANNER,
-  });
+const MainLayoutHeader = () => {
+  const { data } = useQuery<GetTopBannerQuery>(GET_TOP_BANNER);
 
   const _item = data?.sliderCategories?.nodes?.[0]?.sliders?.edges?.[0]?.node;
+  let banner: ISliderItem | null = null;
   if (_item && _item?.featuredImage?.node.url) {
-    const data: ISliderItem = {
+    banner = {
       id: _item.id,
       imageUrl: _item.featuredImage?.node.url,
       url: _item.url,
       title: _item.title!,
     };
-    return data;
   }
-
-  return null;
-};
-
-const MainLayoutHeader = async () => {
-  const topBanner = await getTopBanner();
 
   return (
     <Header>
-      {topBanner && <TopBanner data={topBanner} />}
+      {banner && <TopBanner data={banner} />}
       <MobileView>
         <MobileHeader />
       </MobileView>
